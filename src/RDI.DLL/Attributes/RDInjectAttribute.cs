6804fc11ea1ab4f6414bdd8acddffa70acd23014
Ihar <ihar.krasnik@gmail.com>
2013-08-26 14:58:42 +0300

    Add Code to binary files

---
+++ src/RDI.DLL/Attributes/RDInjectAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDI.Attributes
+{
+    [AttributeUsage(AttributeTargets.Constructor|AttributeTargets.Property|AttributeTargets.Parameter)]
+    public class RDInjectAttribute : Attribute
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/RDI.DLL/Concrete/Containers/RdiSettingsContainer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using RDI.Interfaces.Containers;
+
+namespace RDI.Concrete.Containers
+{
+    public class RdiSettingsContainer : IRdiSettingsContainer
+    {
+        public Type InterfaceType { get; set; }
+        public Type ResolvedType { get; set; }
+        public IRdiBindingSettings BindingSettings { get; protected set; }
+
+        public RdiSettingsContainer(IRdiBindingSettings settings = null)
+        {
+            BindingSettings = settings;
+        }
+
+        public IRdiBindingSettings With<TConcrete>()
+        {
+            Type concreteType = typeof(TConcrete);
+            if (InterfaceType.IsAssignableFrom(concreteType))
+            {
+                ResolvedType = concreteType;
+                return BindingSettings = new RdiBindingSettings();
+            }
+            else
+            {
+                throw new InvalidCastException(String.Format("type {0} is not assignable from type {1}", 
+                                               concreteType.ToString(),InterfaceType.ToString()));
+            }
+        }
+
+
+    }
+}
